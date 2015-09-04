c6e63ed31bfdf316ad367b3312487a7a09aabc03
Ugur Temiz <seugurtemiz@gmail.com>
2015-09-04 22:47:36 +0300

    bonjour and tcp connection is integrated.

--- UIChatBubble/UIChatBubble/UIChatBubbleTableView/UIChatBubbleTableViewController.swift
+++ UIChatBubble/UIChatBubble/UIChatBubbleTableView/UIChatBubbleTableViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import CocoaAsyncSocket
 
-class UIChatBubbleTableViewController: UITableViewController, UITableViewDataSource, UITextFieldDelegate
+enum TAG: Int {
+    case header = 1
+    case body   = 2
+}
+
+class UIChatBubbleTableViewController: UITableViewController, UITableViewDataSource, UITextFieldDelegate,
+                                       NSNetServiceDelegate, NSNetServiceBrowserDelegate, GCDAsyncSocketDelegate
 {
+    var service : NSNetService!
+    var socket  : GCDAsyncSocket!
+    
     var cellDataArray = [ChatBubbleCellData]()
     var ifCellRegistered = false
     @IBOutlet weak var senderTextField: UITextField!
@@ -20,6 +30,7 @@ class UIChatBubbleTableViewController: UITableViewController, UITableViewDataSou
         self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
         loadTestData()
         senderTextField.delegate = self
+        startTalking()
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -27,14 +38,111 @@ class UIChatBubbleTableViewController: UITableViewController, UITableViewDataSou
         senderTextField.becomeFirstResponder()
     }
     
-    //Send button on keyboard action
+    // Send button on keyboard action
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         senderTextField.resignFirstResponder()
-        addMessage(senderTextField.text, date: NSDate(timeIntervalSinceNow: -24*60*60), type: ChatBubbleMessageType.MyMessage)
-        tableView.reloadData()
-        senderTextField.text = ""
+        if sendText() {
+            addMessage(senderTextField.text, date: NSDate(timeIntervalSinceNow: -24*60*60), type: ChatBubbleMessageType.MyMessage)
+            tableView.reloadData()
+            senderTextField.text = ""
+        }
         return true
     }
+    
+    
+    func parseHeader(data: NSData) -> UInt {
+        var out: UInt = 0
+        data.getBytes(&out, length: sizeof(UInt))
+        return out
+    }
+    
+    func handleResponseBody(data: NSData) {
+        var receivedText : String
+        if let message = NSString(data: data, encoding: NSUTF8StringEncoding) {
+            receivedText = message as String
+            addMessage(receivedText, date: NSDate(timeIntervalSinceNow: -24*60*60+30), type: ChatBubbleMessageType.YourMessage)
+            tableView.reloadData()
+        }
+    }
+
+    func sendText() -> Bool{
+        if let data = self.senderTextField.text.dataUsingEncoding(NSUTF8StringEncoding) {
+            var header = data.length
+            let headerData = NSData(bytes: &header, length: sizeof(UInt))
+            self.socket.writeData(headerData, withTimeout: -1.0, tag: TAG.header.rawValue)
+            self.socket.writeData(data, withTimeout: -1.0, tag: TAG.body.rawValue)
+            return true
+        }
+        return false
+    }
+
+    
+    func startTalking () {
+        self.socket = GCDAsyncSocket(delegate: self, delegateQueue: dispatch_get_main_queue())
+        var error : NSError?
+        if self.socket.acceptOnPort(0, error: &error){
+            self.service = NSNetService(domain: "local.", type: "_ugurtemiz._tcp", name: "Ugur's iPhone", port: Int32(self.socket.localPort))
+            self.service.delegate = self
+            self.service.publish()
+        } else {
+            println("Error occured with acceptOnPort. Error \(error)")
+        }
+    }
+    
+    /*
+    *  Delegates of NSNetService
+    **/
+    
+    func netServiceDidPublish(sender: NSNetService) {
+        println("Bonjour service published. domain: \(sender.domain), type: \(sender.type), name: \(sender.name), port: \(sender.port)")
+    }
+    
+    func netService(sender: NSNetService, didNotPublish errorDict: [NSObject : AnyObject]) {
+        println("Unable to create socket. domain: \(sender.domain), type: \(sender.type), name: \(sender.name), port: \(sender.port), Error \(errorDict)")
+    }
+    
+    /*
+    *  END OF Delegates
+    **/
+    
+    /*
+    *  Delegates of GCDAsyncSokcket
+    **/
+    
+    func socket(sock: GCDAsyncSocket!, didAcceptNewSocket newSocket: GCDAsyncSocket!) {
+        println("Did accept new socket")
+        self.socket = newSocket
+        
+        self.socket.readDataToLength(UInt(sizeof(UInt64)), withTimeout: -1.0, tag: 0)
+        println("Connected to " + self.service.name)
+    }
+    
+    func socketDidDisconnect(sock: GCDAsyncSocket!, withError err: NSError!) {
+        println("Socket disconnected: error \(err)")
+        if self.socket == socket {
+            println("Disconnected from " + self.service.name)
+        }
+    }
+    
+    func socket(sock: GCDAsyncSocket!, didReadData data: NSData!, withTag tag: Int) {
+        if data.length == sizeof(UInt) {
+            let bodyLength: UInt = self.parseHeader(data)
+            sock.readDataToLength(bodyLength, withTimeout: -1, tag: TAG.body.rawValue)
+        } else {
+            self.handleResponseBody(data)
+            sock.readDataToLength(UInt(sizeof(UInt)), withTimeout: -1, tag: TAG.header.rawValue)
+        }
+    }
+    
+    func socket(sock: GCDAsyncSocket!, didWriteDataWithTag tag: Int) {
+        println("Write data with tag of \(tag)")
+    }
+    
+    /*
+    *  END OF Delegates
+    **/
+
+    
     
     // Number of rows in TableView
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
